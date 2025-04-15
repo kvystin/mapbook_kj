8777b982359775864e9c0ecc7838c1f8af89079b
kvystin <krystian.jezierski9@gmail.com>
2025-04-15 17:27:38 +0200

    chore: the code was restructured according to the mvc template

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from utlis.model import users
+from utlis.controller import get_user_info
 
 
-users:list=[
-    {"name":"Maciej","location":"Łódź","posts":100},
-    {"name":"Mateusz","location":"Poznań","posts":200},
-    {"name":"Maciej_1","location":"Siedlce","posts":300},
-    {"name":"Konrad","location":"Grudziądz","posts":400},
-]
 
 
-def gets_user_info(users_data:list)->None:
-    for user in users_data:
-        print(f"Twój znajomy {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów.")
+def main():
+    get_user_info(users)
 
-gets_user_info(users)
+
+
+if __name__ == '__main__':
+    main()
