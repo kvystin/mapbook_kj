47084261f46b70a569ed1dd2db10741edeec7b3f
kvystin <krystian.jezierski9@gmail.com>
2025-04-15 17:53:38 +0200

    wip: add MENU

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from utlis.model import users
 from utlis.controller import get_user_info
 
 
-
-
 def main():
-    get_user_info(users)
-
+    print("=============MENU=============")
+    print("0 - zakończ program")
+    print("1 - wyświetl co u znajomych")
+    print("==============================")
+    while True:
+        choice: str = input("wybierz opcje MENU: ")
+        if choice == "0": break
+        if choice == "1": get_user_info(users)
 
 
 if __name__ == '__main__':
